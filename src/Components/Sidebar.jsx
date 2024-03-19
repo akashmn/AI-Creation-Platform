e9aa7fdf348d298a2eeeaef1b7d1bb2200a79f9d
Akash M Nandan <akash003nandan@gmail.com>
2024-03-19 23:34:46 +0530

    sidebar structure set

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,78 @@
-
+import groupIcon from '../assets/group-icon.jpg'
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside>
+
+        {/* Group details section */}
+
+       <div className="groupname">
+            <img src={groupIcon} alt="group-icon" />
+            <div>
+                <p>Intellisys</p>
+                <p>12 member</p>
+            </div>
+       </div>
+
+         {/* General section */}
+       <div className='general'>
+            <p>General</p>
+            <div>
+                <div>
+                    <img src="" alt="search" />
+                    <p>Search</p>
+                </div>
+                <div>
+                    <img src="" alt="billing" />
+                    <p>Billing</p>
+                </div>
+            </div>
+       </div>
+
+        {/* Projects section */}
+       
+       <div className="projects">
+
+            <p>Projects</p>
+            <div>
+                <div>
+                    <img src="" alt="project" />
+                    <p>Orbital Oddysey</p>
+                </div>
+                <div>
+                    <img src="" alt="project" />
+                    <p>Digital Product Launch</p>
+                </div>
+                <div>
+                    <img src="" alt="project" />
+                    <p>Brand Refresh</p>
+                </div>
+                <div>
+                    <img src="" alt="project" />
+                    <p>Social Media Strategy</p>
+                </div>
+            </div>
+            <div className="add-project">
+                <img src="" alt="project" />
+                <p>Orbital Oddysey</p>
+            </div>
+       </div>
+
+       {/* user section */}
+
+        <div className="user">
+            <div>
+                <img src="" alt="user" />
+                <div>
+                    <p>John Doe</p>
+                    <p>Premium</p>
+                </div>
+            </div>
+            <div className="settings">
+                <img src="" alt="settings" />
+            </div>
+        </div>
+
+    </aside>
   )
 }
 
