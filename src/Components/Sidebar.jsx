f6939af55ee48e34cd757ba90a2901c877f51087
Akash M Nandan <akash003nandan@gmail.com>
2024-03-20 02:10:41 +0530

    border set for each children in sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,79 +4,81 @@ import dropdown from '../assets/dropdown.png'
 
 const Sidebar = () => {
   return (
-    <aside className='h-screen w-64 bg-noble-black-800 flex-10 flex-col p-3 rounded-xl'>
+    <aside className='h-screen w-72 bg-noble-black-800 rounded-xl'>
+        <nav className='h-full flex flex-col divide-y-0 divide-noble-black-700'>
+             {/* Group details section */}
+            <div className="flex flex-row items-center justify-between h-[96px] mx-4">
+                <div className='flex flex-row gap-3'>
+                    <img src={groupIcon} alt="group-icon" className='h-11 rounded-2xl' />
+                    <div>
+                        <p className='text-sm'>Intellisys</p>
+                        <p className='text-xs text-stem-green-500'>12 members</p>
+                    </div>
+                 </div>
+                <img src={dropdown} alt="dropdwon" className='h-2' />
+            </div>
 
-        {/* Group details section */}
-
-       <div className="flex flex-row items-center justify-between">
-            <div className='flex flex-row gap-3'>
-                <img src={groupIcon} alt="group-icon" className='h-11 rounded-2xl' />
-                <div>
-                    <p className='text-[15px]'>Intellisys</p>
-                    <p className='text-xs text-stem-green-500'>12 members</p>
+            {/* General section */}
+            <div className='h-[194px]'>
+                <div className='mx-4 my-4 flex flex-col gap-4'>
+                    <p className='text-xs text-noble-black-400'>GENERAL</p>
+                    <div>
+                        <div>
+                            <img src="" alt="search" />
+                            <p>Search</p>
+                        </div>
+                        <div>
+                            <img src="" alt="billing" />
+                            <p>Billing</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <img src={dropdown} alt="dropdwon" className='h-2' />
-       </div>
 
-         {/* General section */}
-       <div className='general'>
-            <p>GENERAL</p>
-            <div>
+            {/* Projects section */}
+        
+            <div className="projects">
+
+                <p>Projects</p>
                 <div>
-                    <img src="" alt="search" />
-                    <p>Search</p>
+                    <div>
+                        <img src="" alt="project" />
+                        <p>Orbital Oddysey</p>
+                    </div>
+                    <div>
+                        <img src="" alt="project" />
+                        <p>Digital Product Launch</p>
+                    </div>
+                    <div>
+                        <img src="" alt="project" />
+                        <p>Brand Refresh</p>
+                    </div>
+                    <div>
+                        <img src="" alt="project" />
+                        <p>Social Media Strategy</p>
+                    </div>
                 </div>
-                <div>
-                    <img src="" alt="billing" />
-                    <p>Billing</p>
-                </div>
-            </div>
-       </div>
-
-        {/* Projects section */}
-       
-       <div className="projects">
-
-            <p>Projects</p>
-            <div>
-                <div>
+                <div className="add-project">
                     <img src="" alt="project" />
                     <p>Orbital Oddysey</p>
                 </div>
-                <div>
-                    <img src="" alt="project" />
-                    <p>Digital Product Launch</p>
-                </div>
-                <div>
-                    <img src="" alt="project" />
-                    <p>Brand Refresh</p>
-                </div>
-                <div>
-                    <img src="" alt="project" />
-                    <p>Social Media Strategy</p>
-                </div>
             </div>
-            <div className="add-project">
-                <img src="" alt="project" />
-                <p>Orbital Oddysey</p>
-            </div>
-       </div>
 
-       {/* user section */}
+            {/* user section */}
 
-        <div className="user">
-            <div>
-                <img src="" alt="user" />
+            <div className="user">
                 <div>
-                    <p>John Doe</p>
-                    <p>Premium</p>
+                    <img src="" alt="user" />
+                    <div>
+                        <p>John Doe</p>
+                        <p>Premium</p>
+                    </div>
+                </div>
+                <div className="settings">
+                    <img src="" alt="settings" />
                 </div>
             </div>
-            <div className="settings">
-                <img src="" alt="settings" />
-            </div>
-        </div>
+        </nav>
 
     </aside>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        'noble-blue-400': '#686B6E',
+        'noble-black-400': '#686B6E',
         'noble-black-700': '#131619',
         'noble-black-800': '#0D0F10',
         'stem-green-500': '#B6F09C',
 
       },
+      borderWidth:{
+      '0': '1.1px',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      }
     },
   },
   plugins: [],
