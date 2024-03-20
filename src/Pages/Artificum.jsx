00f5645c51a08d27e4ce70e61719df00b33f1c46
Akash M Nandan <akash003nandan@gmail.com>
2024-03-20 23:10:32 +0530

    Topbar.jsx created

--- src/Pages/Artificum.jsx
+++ src/Pages/Artificum.jsx
@@ -1,10 +1,13 @@
 import Sidebar from "../Components/Sidebar"
+import Topbar from "../Components/Topbar"
+
 
 
 const Artificum = () => {
   return (
-    <div>
-        <Sidebar/>
+    <div className="flex flex-row">
+        <Sidebar />
+        <Topbar />
     </div>
   )
 }
